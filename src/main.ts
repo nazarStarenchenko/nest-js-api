dfe463c658f1770bc03ac40018b76348d0b3a2c9
nazarStarenchenko <nazarstarenchenko@gmail.com>
2023-02-19 03:20:56 +0200

    disabled cors

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-  	origin: "*",
-  	methods: "GET,HEAD,PUT,PATCH,POST,DELETE"
+  	origin: false,
   });
 
   await app.listen(8080);
