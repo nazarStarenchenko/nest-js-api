747fcee9329e99caec8b5d06b8cd998eb72abfe4
nazarStarenchenko <nazarstarenchenko@gmail.com>
2023-02-19 17:48:49 +0200

    changed origin address

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-	  origin: 'https://nazarstarenchenko.github.io/',
+	  origin: 'https://nazarstarenchenko.github.io',
   });
   await app.listen(8080);
 }
