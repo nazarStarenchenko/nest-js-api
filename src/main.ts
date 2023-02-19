83cb4018c38f893680226924595b6f930e19f19d
nazarStarenchenko <nazarstarenchenko@gmail.com>
2023-02-19 17:25:37 +0200

    deleted cors once and for all

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-  	origin: false,
-  });
-
   await app.listen(8080);
 }
 bootstrap();
