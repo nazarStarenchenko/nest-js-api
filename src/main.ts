1aa6a8aa5ce2982fc59681ce15f3c974bd63e5fb
nazarStarenchenko <nazarstarenchenko@gmail.com>
2023-02-19 03:03:13 +0200

    site changed

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: "https://nazarstarenchenko.github.io/form-test-react/",
-  });
   await app.listen(8080);
 }
 bootstrap();
